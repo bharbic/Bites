129f56c305b8cab10783d7d53b4f38b10b3a7861
bharbic <benjaminharbic@gmail.com>
2025-05-11 21:09:46 +0200

    first changes

--- app/src/main/java/com/example/bites/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/bites/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Order from your favorite stores in minutes."
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/bites/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/bites/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "No active deliveries yet. Start by going to the dashboard!"
     }
     val text: LiveData<String> = _text
 }
